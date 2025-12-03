0496dcda0ef62e74a19206c726c0fa37660f4206
James Jones <velocity303@gmail.com>
2025-12-02 20:26:33 -0500

    update build logic

--- install_websocket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-echo "==> [Websocket] Starting Installation..."
-pip3 install --verbose --prefix=/app .
-echo "==> [Websocket] Checking installation..."
-find /app -type d -name "websocket"
